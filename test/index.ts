2f7f75926eedbfebb96cb8736e3ab9cb2bc28d38
Patrik Oldsberg <poldsberg@gmail.com>
2022-06-22 14:55:58 +0200

    ts setup 🎉
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Co-authored-by: blam <ben@blam.sh>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -11,5 +11,5 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(String(error));
 }
