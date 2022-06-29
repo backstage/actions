88904b08aee1f5e38fd7cda0f5861f3fc0f83fd9
Patrik Oldsberg <poldsberg@gmail.com>
2022-06-29 18:28:12 +0200

    use app installation id as well + 0.1.12

--- lib/createAppClient.ts
+++ lib/createAppClient.ts
@@ -5,15 +5,14 @@ import { createAppAuth } from '@octokit/auth-app';
 export function createAppClient() {
   const appId = core.getInput('app-id', { required: true });
   const privateKey = core.getInput('private-key', { required: true });
-
-  core.info(`appId: ${appId}`);
-  core.setSecret(privateKey);
+  const installationId = core.getInput('installation-id', { required: true });
 
   return new GitHub({
     authStrategy: createAppAuth,
     auth: {
       appId,
       privateKey,
+      installationId,
     },
   });
 }
