d54158e72d7f75a5d67e35a79f36a2d92ea14425
Patrik Oldsberg <poldsberg@gmail.com>
2022-07-01 11:45:33 +0200

    pr-sync: add initial project board sync
    Co-authored-by: blam <ben@blam.sh>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>

--- pr-sync/index.ts
+++ pr-sync/index.ts
@@ -2,24 +2,50 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { mkLog } from '../lib/mkLog';
 import { approveRenovatePRs } from './approveRenovatePRs';
+import { syncProjectBoard } from './syncProjectBoard';
 
 async function main() {
   core.info(`Running pr-sync!`);
   const token = core.getInput('github-token', { required: true });
+  const boardNumberStr = core.getInput('board-number', { required: true });
   const client = github.getOctokit(token);
   const repoInfo = github.context.repo;
+  const action = github.context.payload.action;
 
-  await Promise.all([
-    approveRenovatePRs(
+  const boardNumber = parseInt(boardNumberStr, 10);
+  if (Number.isNaN(boardNumber)) {
+    core.setFailed(`Invalid board number: ${boardNumberStr}`);
+    return;
+  }
+
+  const promises = [
+    syncProjectBoard(
       client,
       {
         ...repoInfo,
         issueNumber: github.context.issue.number,
-        actor: github.context.actor,
+        boardNumber,
+        action,
       },
       mkLog('approve-renovate-prs'),
     ),
-  ]);
+  ];
+
+  if (action !== 'closed') {
+    promises.push(
+      approveRenovatePRs(
+        client,
+        {
+          ...repoInfo,
+          issueNumber: github.context.issue.number,
+          actor: github.context.actor,
+        },
+        mkLog('approve-renovate-prs'),
+      ),
+    );
+  }
+
+  await Promise.all(promises);
 }
 
 main().catch(error => {
---
+++ pr-sync/syncProjectBoard.ts
@@ -0,0 +1,69 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Repository, Project, Organization } from '@octokit/graphql-schema';
+
+interface Options {
+  owner: string;
+  repo: string;
+  issueNumber: number;
+  boardNumber: number;
+  action?: string;
+}
+
+export async function syncProjectBoard(
+  client: ReturnType<typeof github.getOctokit>,
+  options: Options,
+  log = core.info,
+) {
+  if (options.action === 'closed') {
+    await removeFromBoard(client, options, log);
+  }
+}
+
+export async function removeFromBoard(
+  client: ReturnType<typeof github.getOctokit>,
+  options: Options,
+  log = core.info,
+) {
+  log(`Removing issue ${options.issueNumber} from board`);
+
+  const data = await client.graphql<{ organization?: Organization }>(
+    `
+query ($owner: String!, $repo: String!, $issueNumber: Int!) {
+  organization(login: $owner) {
+    repository(name: $repo) {
+      pullRequest(number: $issueNumber) {
+        projectItems(first: 10) {
+          nodes {
+            id
+            project {
+              id
+              number
+            }
+          }
+        }
+      }
+    }
+  }
+}`,
+    { ...options },
+  );
+
+  const items =
+    data.organization?.repository?.pullRequest?.projectItems?.nodes ?? [];
+
+  const item = items.find(i => i?.project?.number === options.boardNumber);
+  if (!item) {
+    return;
+  }
+
+  await client.graphql<{ repository?: Repository }>(
+    `
+mutation {
+  deleteProjectV2Item(input:{ projectId: $projectId, itemId: $itemId }) {
+    deletedItemId
+  }
+}`,
+    { projectId: item.project.id, itemId: item.id },
+  );
+}
