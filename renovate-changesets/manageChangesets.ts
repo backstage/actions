ee3524b79738b6fe8459a8195bd1ec5a279d11fd
Scott Guymer <scott.guymer@philips.com>
2024-05-05 18:22:44 +0200

    feat: Enable renovate changeset action to work with dependabot

--- renovate-changesets/manageChangesets.ts
+++ renovate-changesets/manageChangesets.ts
@@ -2,6 +2,10 @@ import { getExecOutput, exec } from '@actions/exec';
 import fs from 'fs/promises';
 import { resolve as resolvePath, relative as relativePath } from 'path';
 import { getPackages, type Package } from '@manypkg/get-packages';
+import {
+  dependencyMangerConfig,
+  getDependencyManager,
+} from './dependencyConfig';
 
 export async function getBranchName() {
   const { stdout } = await getExecOutput('git', ['branch', '--show-current']);
@@ -34,7 +38,9 @@ export async function getChangesetFilename() {
   const { stdout: shortHash } = await getExecOutput(
     'git rev-parse --short HEAD',
   );
-  return `.changeset/renovate-${shortHash.trim()}.md`;
+  return `.changeset/${
+    dependencyMangerConfig[getDependencyManager()].changesetPrefix
+  }-${shortHash.trim()}.md`;
 }
 
 export async function createChangeset(
